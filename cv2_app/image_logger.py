0689f71ac3558d449f9b000b5fc853f913a6e9ee
ohilho <iroiro515@gmail.com>
2022-06-17 13:51:21 +0900

    add log_image_with_app function to the image_logger and new example is created

--- cv2_app/image_logger.py
+++ cv2_app/image_logger.py
@@ -2,11 +2,15 @@
 
 import cv2
 from pathlib import Path
-
+from .cv2_app import CV2App
+from typing import Union
 
 class ImageLogger:
-    def __init__(self, out_dir) -> None:
+    def __init__(self, out_dir, app:Union[CV2App,None] = None) -> None:
         self.path = Path(out_dir)
+        self.app = app
+        if app:
+            app.register_render_callback(self.log_image_with_app,[self.app]) 
 
     def log_image_with_name(self, filename, image) -> None:
         self.path.mkdir(parents=True, exist_ok=True)
@@ -14,3 +18,9 @@ class ImageLogger:
 
     def log_image_with_sequence(self, seq, image) -> None:
         self.log_image_with_name(f"{seq:06d}.png", image)
+
+    def log_image_with_time(self, time, image) -> None:
+        self.log_image_with_name(f"{time:.6f}.png", image)
+
+    def log_image_with_app(self,app:CV2App) -> None:
+        self.log_image_with_name(f"{app.current_seq:06d}.png", app.processed_image)
---
+++ video_frame_extractor2.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python3
+
+from cv2_app.cv2_app import CV2App
+from cv2_app.image_loader import VideoFrameLoader
+from cv2_app.image_logger import ImageLogger
+from argparse import ArgumentParser
+import cv2
+
+def image_resizer(app:CV2App):
+    app.processed_image = cv2.resize(app.current_image,dsize=None, fx=0.5,fy=0.5)
+    
+def main():
+    # get arguemnts. 
+    parser = ArgumentParser("frame_extractor")
+    parser.add_argument("--file", type=str, required=True, help="path to the video file")
+    parser.add_argument("--out", type=str, required=True, help="path to the output files")
+
+    args = parser.parse_args()
+    print(f"start capturing {args.file} ...")
+
+    
+    # create app
+    view = CV2App()
+    
+    # create and set image loader. in this case, video frame loader
+    view.set_image_loader(VideoFrameLoader(args.file))
+    
+    # add key event. this video window will be destoryed when q is pressed.
+    view.register_key_event("q",CV2App.quit)
+    # do nothing but just activate key match for iterating frame. simply speaking, this is "next frame" button.
+    view.register_key_event("n",lambda : None) 
+    
+    # image will be modified with this callback. 
+    # Image resizer. 
+    view.register_render_callback(image_resizer,[view]) 
+
+    # create image logger
+    # this time, image logger will register callback with app automatically.
+    lg = ImageLogger(args.out, view)
+    
+    # play video with 1 ms interval between each frame. 
+    view.start(1, frame_controled_by_key=False)
+    
+    # or you can control your image sequence by pressing 'n' key
+    # To do so, change frame_controled_by_key from False to True.
+    # view.start(33, frame_controled_by_key=True)
+    
+
+
+if __name__ == "__main__":
+    main()
+    
